820458ab1cb8aa5efe9be9e3b06d1e558f57b6af
Vadim Turok <turokvadim2510@gmail.com>
2021-05-20 23:28:00 +0300

    added without.js

---
+++ without.js
@@ -0,0 +1,20 @@
+function without(array, ...values){
+    let newArray = []
+    let toArray = false
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < values.length; j++){
+            if(array[i] !== values[j]){
+                toArray = true
+            }else{
+                toArray = false
+                break
+            }
+        }
+        if(toArray){
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
+
+console.log(without([2, 1, 2, 3], 1, 2));
